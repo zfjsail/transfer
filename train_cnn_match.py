95a505f759232482dce7b1f27506ad47550971c2
Fanjin Zhang <zfjsail@163.com>
2020-08-11 10:54:05 +0800

    larger lr

--- train_cnn_match.py
+++ train_cnn_match.py
@@ -45,7 +45,7 @@ parser.add_argument('--build-index-window', type=int, default=5, help='Matrix2 h
 parser.add_argument('--seed', type=int, default=42, help='Random seed.')
 parser.add_argument('--seed-delta', type=int, default=0, help='Random seed.')
 parser.add_argument('--epochs', type=int, default=500, help='Number of epochs to train.')
-parser.add_argument('--lr', type=float, default=1e-3, help='Initial learning rate.')
+parser.add_argument('--lr', type=float, default=5e-3, help='Initial learning rate.')
 parser.add_argument('--initial-accumulator-value', type=float, default=0.01, help='Initial accumulator value.')
 parser.add_argument('--weight-decay', type=float, default=1e-3,
                     help='Weight decay (L2 loss on parameters).')
